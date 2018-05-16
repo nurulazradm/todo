f699b6ebc4ea093aa16062c66cc992a594ff2b6e
Nurulazrad M <nurulazrad@gmail.com>
2018-05-16 09:55:09 +0800

    Moved styling to helper and add strikethrough text

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,13 @@
 module ListsHelper
+  def completed_class list
+    if list.completed?
+      'table-success'
+    end
+  end
+
+  def striker_class list
+    if list.completed?
+      'striker'
+    end
+  end
 end
