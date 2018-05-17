afd0fc9922639ccbad3990e9026759ce42388e96
Nurulazrad M <nurulazrad@gmail.com>
2018-05-17 10:37:50 +0800

    Delete all items

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,9 @@ class HomeController < ApplicationController
     List.marked_items.destroy_all
     redirect_to lists_url, notice: 'Marked Items Were Successfully Deleted.'
   end
+
+  def empty_list
+    List.all.destroy_all
+    redirect_to lists_url, notice: 'Todo Items Were Successfully Deleted.'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   root 'lists#index'
   get 'home/empty_trash'
+  get 'home/empty_list'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
